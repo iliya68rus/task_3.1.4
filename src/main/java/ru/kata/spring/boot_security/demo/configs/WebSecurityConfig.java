7c3821741ecf7af2ba8ef125b1e476c2ff46d8c7
iliya68rus <iliya97pro@yandex.ru>
2022-05-14 21:45:42 +0300

    код стайл

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -4,16 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.filter.HiddenHttpMethodFilter;
 import ru.kata.spring.boot_security.demo.services.CustomUserDetailsService;
-
-import javax.servlet.ServletContext;
 
 @Configuration
 @EnableWebSecurity
@@ -34,17 +30,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//                .withUser("user")
-//                .password("user")
-//                .authorities("ROLE_USER")
-//                .and()
-//                .withUser("admin")
-//                .password("admin")
-//                .authorities("ROLE_USER");
-
-//        auth.jdbcAuthentication()
-//                .;
         auth.userDetailsService(userDetailsService);
 
     }
@@ -52,38 +37,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-//                .csrf().disable()
-//                .csrf().and()
                 .authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/user/**").hasAnyRole("ADMIN", "USER")
                 .antMatchers("/**").permitAll()
                 .and()
                 .formLogin().successHandler(successUserHandler);
-//
-////                .authorizeRequests()
-////                .antMatchers("/", "/index").permitAll()
-////                .anyRequest().authenticated()
-////                .and()
-////                .formLogin().successHandler(successUserHandler)
-////                .permitAll()
-////                .and()
-////                .logout()
-////                .permitAll();
     }
-
-    // аутентификация inMemory
-//    @Bean
-//    @Override
-//    public UserDetailsService userDetailsService() {
-//        UserDetails user =
-//                User.withDefaultPasswordEncoder()
-//                        .username("user")
-//                        .password("user")
-//                        .roles("USER")
-//                        .build();
-//
-//        return new InMemoryUserDetailsManager(user);
-//        return userDetailsService();
-//    }
 }
