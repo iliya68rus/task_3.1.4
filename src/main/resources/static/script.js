a63e74506cb128869abb67961fa84e55cf926c51
iliya68rus <iliya97pro@yandex.ru>
2022-05-19 16:42:37 +0300

    стабильно

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -31,112 +31,62 @@ function edit(id) {
     fetch('/api/users/' + id)
         .then(result => result.json())
         .then(user => {
-            tempModal = '<div class="modal fade" id="edituser" tabIndex="-1" role="dialog" ' +
-                'aria-labelledby="exampleModalLabel" aria-hidden="true">' +
-                '<div class="modal-dialog" role="document">' +
-                '<div class="modal-content">' +
-                '<div class="modal-header">' +
-                '<h5 class="modal-title" id="exampleModalLabel">Edit user</h5>' +
-                '<button type="button" class="close" data-dismiss="modal" ' +
-                'aria-label="Close">' +
-                '<span aria-hidden="true">&times;</span>' +
-                '</button>' +
-                '</div>' +
-                '<div class="modal-body">' +
-                '<div class="container-fluid">' +
-                '<div class="row">' +
-                '<div class="col-sm-3">' +
-                '</div>' +
-                '<div class="col-sm-6 text-center">' +
-                '<form method="PUT">' +
-                '<div class="form-group">' +
-                '<label for="edit1">' +
-                '<b>' +
-                'ID' +
-                '</b>' +
-                '</label>' +
-                '<input type="text" ' +
-                'class="form-control" id="edit1" name="id" value="' + user.id + '" readonly>' +
-                '</div>' +
-                '<div class="form-group">' +
-                '<label for="edit2">' +
-                '<b>' +
-                'Name' +
-                '</b>' +
-                '</label>' +
-                '<input type="text" ' +
-                'value="' + user.name + '" ' +
-                'class="form-control" id="edit2" name="Name" ' +
-                'placeholder="Name">' +
-                '</div>' +
-                '<div class="form-group">' +
-                '<label for="edit3">' +
-                '<b>' +
-                'Last name' +
-                '</b>' +
-                '</label>' +
-                '<input type="text" ' +
-                'class="form-control" id="edit3" value="' + user.lastName + '" name="lastName" ' +
-                'placeholder="Last name">' +
-                '</div>' +
-                '<div class="form-group">' +
-                '<label for="edit4">' +
-                '<b>' +
-                'Age' +
-                '</b>' +
-                '</label>' +
-                '<input type="number" ' +
-                'class="form-control" id="edit4" value="' + user.age + '" name="age" ' +
-                'placeholder="Age">' +
-                '</div>' +
-                '<div class="form-group">' +
-                '<label for="edit5">' +
-                '<b>' +
-                'Email' +
-                '</b>' +
-                '</label>' +
-                '<input type="text" ' +
-                'class="form-control" id="edit5" value="' + user.email + '" name="email" ' +
-                'placeholder="Email">' +
-                '</div>' +
-                '<div class="form-group">' +
-                '<label for="edit6">' +
-                '<b>' +
-                'Password' +
-                '</b>' +
-                '</label>' +
-                '<input type="password" class="form-control" name="password" ' +
-                'id="edit6" value="' + user.password + '">' +
-                '</div>' +
-                '<div class="form-group">' +
-                '<label for="select2">' +
-                '<b>' +
-                'Role' +
-                '</b>' +
-                '</label>' +
-                '<select multiple class="form-control" size="2" ' +
-                'name="roles" id="select2">' +
-                '<option value="ROLE_ADMIN">ADMIN</option>' +
-                '<option value="ROLE_USER">USER</option>' +
-                '</select>' +
-                '</div>' +
-                '</form>' +
-                '</div>' +
-                '<div class="col-sm-3">' +
-                '</div>' +
-                '</div>' +
-                '</div>' +
-                '</div>' +
-                '<div class="modal-footer">' +
-                '<button type="button" class="btn btn-secondary" ' +
-                'data-dismiss="modal">Close' +
-                '</button>' +
-                '<button type="submit" class="btn btn-primary" onclick="editSubmit()" data-dismiss="modal">Edit</button>' +
-                '</div>' +
-                '</div>' +
-                '</div>' +
-                '</div>'
-
+            tempModal = `
+            <div class="modal fade" id="edituser" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Edit user</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"> 
+                                <span aria-hidden="true"></span> 
+                            </button>
+                        </div>
+                            <div class="modal-body">
+                                <div class="container-fluid">
+                                    <div class="row">
+                                        <div class="col-sm-3">
+                                    </div>
+                                        <div class="col-sm-6 text-center">
+                                            <form method="PUT">
+                                                <div class="form-group">
+                                                    <label for="edit1"><b>ID</b></label>
+                                                    <input type="text" class="form-control" id="edit1" name="id" value="${user.id}" readonly>
+                                                </div>
+                                                <div class="form-group"><label for="edit2"><b>Name</b></label>
+                                                    <input type="text" class="form-control" id="edit2" value="${user.name}" name="Name" >
+                                                </div>
+                                                <div class="form-group"><label for="edit3"><b>Last name</b></label>
+                                                    <input type="text" class="form-control" id="edit3" value="${user.lastName}" name="lastName" >
+                                                </div>
+                                                <div class="form-group"><label for="edit4"><b>Age</b></label>
+                                                    <input type="number" class="form-control" id="edit4" value="${user.age}" name="age" >
+                                                </div>
+                                                <div class="form-group"><label for="edit5"><b>Email</b></label>
+                                                    <input type="text" class="form-control" id="edit5" value="${user.email}" name="email" >
+                                                </div>
+                                                <div class="form-group"><label for="edit6"><b>Password</b></label>
+                                                    <input type="password" class="form-control" name="password" id="edit6" value="${user.password}">
+                                                </div>
+                                                <div class="form-group"><label for="select2"><b>Role</b></label>
+                                                    <select multiple class="form-control" size="2" name="roles" id="select2"> 
+                                                        <option value="ROLE_ADMIN">ADMIN</option>
+                                                        <option value="ROLE_USER">USER</option>
+                                                    </select>
+                                                </div>
+                                            </form>
+                                        </div>
+                                        <div class="col-sm-3"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="submit" class="btn btn-primary" onclick="editSubmit()" data-dismiss="modal">Edit</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `
             document.getElementById('modal1').innerHTML = tempModal
             $("#edituser").modal()
         })
@@ -192,107 +142,65 @@ function deleteUser(id) {
     fetch('/api/users/' + id)
         .then(result => result.json())
         .then(user => {
-            tempModal = '<div class="modal fade" id="deleteuser" tabIndex="-1" role="dialog" ' +
-                'aria-labelledby="exampleModalLabel" aria-hidden="true">' +
-                '<div class="modal-dialog" role="document">' +
-                '<div class="modal-content">' +
-                '<div class="modal-header">' +
-                '<h5 class="modal-title" id="exampleModalLabel">Delete user</h5>' +
-                '<button type="button" class="close" data-dismiss="modal" ' +
-                'aria-label="Close">' +
-                '<span aria-hidden="true">&times;</span>' +
-                '</button>' +
-                '</div>' +
-                '<div class="modal-body">' +
-                '<div class="container-fluid">' +
-                '<div class="row">' +
-                '<div class="col-sm-3">' +
-                '</div>' +
-                '<div class="col-sm-6 text-center">' +
-                '<form method="DELETE">' +
-                '<div class="form-group">' +
-                '<label for="edit1">' +
-                '<b>' +
-                'ID' +
-                '</b>' +
-                '</label>' +
-                '<input type="text" ' +
-                'class="form-control" id="edit1" name="id" value="' + user.id + '" readonly>' +
-                '</div>' +
-                '<div class="form-group">' +
-                '<label for="edit2">' +
-                '<b>' +
-                'First name' +
-                '</b>' +
-                '</label>' +
-                '<input type="text" ' +
-                'value="' + user.name + '" ' +
-                'class="form-control" id="edit2" name="Name" ' +
-                'placeholder="Name" readonly>' +
-                '</div>' +
-                '<div class="form-group">' +
-                '<label for="edit3">' +
-                '<b>' +
-                'Last name' +
-                '</b>' +
-                '</label>' +
-                '<input type="text" ' +
-                'class="form-control" id="edit3" value="' + user.lastName + '" name="lastName" ' +
-                'placeholder="Last name" readonly>' +
-                '</div>' +
-                '<div class="form-group">' +
-                '<label for="edit4">' +
-                '<b>' +
-                'Age' +
-                '</b>' +
-                '</label>' +
-                '<input type="number" ' +
-                'class="form-control" id="edit4" value="' + user.age + '" name="age" ' +
-                'placeholder="Age" readonly>' +
-                '</div>' +
-                '<div class="form-group">' +
-                '<label for="edit5">' +
-                '<b>' +
-                'Email' +
-                '</b>' +
-                '</label>' +
-                '<input type="text" ' +
-                'class="form-control" id="edit5" value="' + user.email + '" name="email" ' +
-                'placeholder="Email" readonly>' +
-                '</div>' +
-                '<div class="form-group">' +
-                '<label for="select2">' +
-                '<b>' +
-                'Role' +
-                '</b>' +
-                '</label>' +
-                '<select multiple class="form-control" size="2" ' +
-                'name="roles" id="select2" disabled>' +
-                '<option value="ROLE_ADMIN">ADMIN</option>' +
-                '<option value="ROLE_USER">USER</option>' +
-                '</select>' +
-                '</div>' +
-                '</form>' +
-                '</div>' +
-                '<div class="col-sm-3">' +
-                '</div>' +
-                '</div>' +
-                '</div>' +
-                '</div>' +
-                '<div class="modal-footer">' +
-                '<button type="button" class="btn btn-secondary" ' +
-                'data-dismiss="modal">Close' +
-                '</button>' +
-                '<button type="submit" class="btn btn-danger" onclick="deleteSubmit(' + id + ')" data-dismiss="modal">Delete</button>' +
-                '</div>' +
-                '</div>' +
-                '</div>' +
-                '</div>'
-
+            tempModal = `
+            <div class="modal fade" id="deleteuser" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Delete user</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"> 
+                                <span aria-hidden="true"></span> 
+                            </button>
+                        </div>
+                            <div class="modal-body">
+                                <div class="container-fluid">
+                                    <div class="row">
+                                        <div class="col-sm-3">
+                                    </div>
+                                        <div class="col-sm-6 text-center">
+                                            <form method="DELETE">
+                                                <div class="form-group">
+                                                    <label for="edit1"><b>ID</b></label>
+                                                    <input type="text" class="form-control" id="edit1" name="id" value="${user.id}" readonly>
+                                                </div>
+                                                <div class="form-group"><label for="edit2"><b>Name</b></label>
+                                                    <input type="text" class="form-control" id="edit2" value="${user.name}" name="Name" readonly>
+                                                </div>
+                                                <div class="form-group"><label for="edit3"><b>Last name</b></label>
+                                                    <input type="text" class="form-control" id="edit3" value="${user.lastName}" name="lastName" readonly>
+                                                </div>
+                                                <div class="form-group"><label for="edit4"><b>Age</b></label>
+                                                    <input type="number" class="form-control" id="edit4" value="${user.age}" name="age" readonly>
+                                                </div>
+                                                <div class="form-group"><label for="edit5"><b>Email</b></label>
+                                                    <input type="text" class="form-control" id="edit5" value="${user.email}" name="email" readonly>
+                                                </div>
+                                                <div class="form-group"><label for="edit6"><b>Password</b></label>
+                                                    <input type="password" class="form-control" name="password" id="edit6" value="${user.password}" readonly>
+                                                </div>
+                                                <div class="form-group"><label for="select2"><b>Role</b></label>
+                                                    <select multiple class="form-control" size="2" name="roles" id="select2" readonly> 
+                                                        <option value="ROLE_ADMIN">ADMIN</option>
+                                                        <option value="ROLE_USER">USER</option>
+                                                    </select>
+                                                </div>
+                                            </form>
+                                        </div>
+                                        <div class="col-sm-3"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="submit" class="btn btn-danger" onclick="deleteSubmit(${user.id})" data-dismiss="modal">Delete</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `
             document.getElementById('modal1').innerHTML = tempModal
             $("#deleteuser").modal()
         })
-
 }
 
 function deleteSubmit(id) {
